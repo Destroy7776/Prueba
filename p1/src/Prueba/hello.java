e53703fbcfd98c4237a2561fedeea858fa25598b
BrayanMD <brayam7776@hotmail.com>
2023-03-13 18:55:37 +0100

    Creo un module, dentro aparece el src y ahí he creado la clase principal y helloWorld
    Suma

--- p1/src/Prueba/hello.java
+++ p1/src/Prueba/hello.java
@@ -1,7 +1,12 @@
 package Prueba;
 
 public class hello {
+
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Suma: "+suma(20,2));
+    }
+    public static int suma(int x, int y){
+        return x+y;
     }
 }
